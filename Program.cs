8cac526194d28deb19c9d114568925af32ec9d34
biacosta7 <contact.beatrizcosta@gmail.com>
2025-07-21 18:03:13 -0300

    TODOs realizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo X", imei: "9999999", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "87654321", modelo: "Modelo y", imei: "0000000", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Google Meet");
